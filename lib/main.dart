fa469fa6ea75061aae6337930ee84608fa009884
Mohamed Ramadan <loaarabaa5544@gmail.com>
2025-07-03 17:55:48 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+void main(){
+  runApp(loaa());
+}
+
+
+class loaa extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+   
+    );
+    
+  }
+}
