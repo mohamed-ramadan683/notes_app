f05ec944d096338a690b3036fda4ebfcb139400b
Mohamed Ramadan <loaarabaa5544@gmail.com>
2025-07-07 14:45:27 +0300

    create add note cubit

---
+++ lib/add_note_cubit/cubit/add_note_cubit_state.dart
@@ -0,0 +1,12 @@
+part of 'add_note_cubit_cubit.dart';
+
+@immutable
+sealed class AddNoteCubitState {}
+
+final class AddNoteCubitInitial extends AddNoteCubitState {}
+final class AddNoteCubitLoading extends AddNoteCubitState {}
+final class AddNoteCubitSuccess extends AddNoteCubitState {}
+final class AddNoteCubitFailure extends AddNoteCubitState {
+  String errmessage;
+  AddNoteCubitFailure({required this.errmessage});
+}
